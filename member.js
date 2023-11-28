89ccee50e01dce13b50a36e193e5129d6bd04027
Jvilini-Agard <140734392+Jvilini-Agard@users.noreply.github.com>
2023-11-28 21:25:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+     let memberSkills = [
+        { category: 'Programming', languages: ['JavaScript', 'Python', 'Java'] },
+        { category: 'Frontend', technologies: ['HTML', 'CSS', 'React'] },
+        { category: 'Backend', frameworks: ['Node.js', 'Express'] },
+        { category: 'Database', databases: ['MongoDB', 'MySQL'] },
+        { category: 'Other', tools: ['Git', 'RESTful APIs'] }
+    ];
+
+    return memberSkills;
+}
